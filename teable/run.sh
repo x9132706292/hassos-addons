506c7ac7ca3b007799b86adcea1d69b4465f4301
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2025-03-06 20:36:21 +0700

    Update run.sh

--- teable/run.sh
+++ teable/run.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$DATABASE_URL" ]; then
+if [ -z $(bashio::config 'database_url') ]; then
   echo "Error: DATABASE_URL is not set. Please configure it in the addon options."
   exit 1
 fi
 
-if [ -z "$PUBLIC_ORIGIN" ]; then
+if [ -z  $(bashio::config 'public_origin') ]; then
   echo "Error: PUBLIC_ORIGIN is not set. Please configure it in the addon options (e.g., http://your-ha-ip:3002)."
   exit 1
 fi
 
-echo "DATABASE_URL: $DATABASE_URL"
-echo "PUBLIC_ORIGIN: $PUBLIC_ORIGIN"
+echo "DATABASE_URL: $(bashio::config 'database_url')"
+echo "PUBLIC_ORIGIN: $(bashio::config 'public_origin')"
 
-until psql "$DATABASE_URL" -c '\q'; do
+until psql $(bashio::config 'database_url') -c '\q'; do
   echo "Waiting for PostgreSQL to be ready..."
   sleep 2
 done
