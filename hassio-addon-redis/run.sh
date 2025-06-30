9f0a75a16a3b891a7baa89706eeed3f25cb943ab
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2025-06-30 12:32:54 +0700

    Update run.sh

--- hassio-addon-redis/run.sh
+++ hassio-addon-redis/run.sh
@@ -1,18 +1,10 @@
-#!/usr/bin/with-contenv bash
+#!/bin/bash
+set -e
 # ==============================================================================
 # Home Assistant Add-on: Redis
 # Runs the Redis server
 # ==============================================================================
 
 CONFIG_PATH=/data/options.json
-REDIS_CONF=/etc/redis.conf
 
-# Get password from options (if set)
-PASSWORD=$(jq --raw-output '.password // empty' $CONFIG_PATH)
-
-# Generate Redis configuration
-if [ -n "$PASSWORD" ]; then
-  echo "requirepass $PASSWORD" > $REDIS_CONF
-else
-  echo "" > $REDIS_CONF
-fi
+export REDIS_PASSWORD=$(jq --raw-output '.password // empty' $CONFIG_PATH)
