42274812886f654ef6bbe7b3e5f5836d2cf3600b
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2025-07-01 21:21:25 +0700

    Update start.sh

--- hassio-addon-teable/start.sh
+++ hassio-addon-teable/start.sh
@@ -7,7 +7,7 @@ eval $(jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "
 
 export PRISMA_DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 echo $PRISMA_DATABASE_URL
-if [$REDIS]; then
+if (($REDIS=="true")) then
     export BACKEND_CACHE_PROVIDER=redis
     export BACKEND_CACHE_REDIS_URI=redis://default:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}/${REDIS_DB}
     echo $BACKEND_CACHE_REDIS_URI
