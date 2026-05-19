a4ed037443b3e9171df2f937ebf59eb502d411fb
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2026-05-19 09:23:56 +0700

    Create docker-edpoint.sh

---
+++ hassio-addon-slash/docker-edpoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+set -e
+
+CONFIG_PATH=/data/options.json
+echo 'Starting with the following configuration:';
+jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "\t" + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH;
+eval $(jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "export " + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH);
+
+./slash
