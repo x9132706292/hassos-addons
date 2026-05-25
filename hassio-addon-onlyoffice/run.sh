400eaa1445cc22e176e318e0f5e2dc98edbbe089
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2026-05-25 19:28:13 +0700

    Update run.sh

--- hassio-addon-onlyoffice/run.sh
+++ hassio-addon-onlyoffice/run.sh
@@ -4,3 +4,5 @@ CONFIG_PATH=/data/options.json
 echo 'Starting with the following configuration:';
 jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "\t" + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH;
 eval $(jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "export " + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH);
+
+/app/ds/run-document-server.sh
