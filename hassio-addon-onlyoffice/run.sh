960fad5d316c611bfd487ca77d31c2c1784bdc96
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2026-05-25 19:07:41 +0700

    Create run.sh

---
+++ hassio-addon-onlyoffice/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+CONFIG_PATH=/data/options.json
+echo 'Starting with the following configuration:';
+jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "\t" + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH;
+eval $(jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "export " + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH);
