3cb3f96d88e4dfbc8127f48f852c40c193227eaf
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2026-06-23 19:39:37 +0700

    Update run.sh

--- hassio-addon-metube/run.sh
+++ hassio-addon-metube/run.sh
@@ -4,3 +4,6 @@ CONFIG_PATH=/data/options.json
 echo 'Starting with the following configuration:';
 jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "\t" + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH;
 eval $(jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "export " + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH);
+
+# Запускаем оригинальный entrypoint от MeTube
+exec /usr/bin/tini -g -- ./docker-entrypoint.sh
