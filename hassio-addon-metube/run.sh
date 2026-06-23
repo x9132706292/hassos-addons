7390c6782c7852e6ff4de6bd8d8a8ebb79d4d063
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2026-06-23 19:37:54 +0700

    Update run.sh

--- hassio-addon-metube/run.sh
+++ hassio-addon-metube/run.sh
@@ -4,5 +4,3 @@ CONFIG_PATH=/data/options.json
 echo 'Starting with the following configuration:';
 jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "\t" + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH;
 eval $(jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "export " + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH);
-
-/app/ds/run-document-server.sh
