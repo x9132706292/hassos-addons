68ed06790fecf9bfd17111a4a2a92345502c08e8
x9132706292 <109609749+x9132706292@users.noreply.github.com>
2026-09-22 10:10:40 +0700

    Update run.sh

--- hassio-addon-ssh-tunnel-manager/run.sh
+++ hassio-addon-ssh-tunnel-manager/run.sh
@@ -1,15 +1,7 @@
-#!/usr/bin/with-contenv bashio
-# shellcheck shell=bash
-set -e
+#!/bin/sh
+set -eu
 
-# Читаем tunnels из конфигурации аддона.
-# Если значение null, заменяем на пустой список.
-TUNNELS_JSON=$(bashio::config 'tunnels' | jq -c 'if . == null then [] else . end')
-
-# Директория для приватных ключей, которые приходят через конфигурацию.
 mkdir -p /data/ssh_keys
 chmod 700 /data/ssh_keys
 
-bashio::log.info "Запуск SSH Tunnel Manager..."
-
-exec python3 /tunnels_manager.py "$TUNNELS_JSON"
+exec python3 /tunnels_manager.py
